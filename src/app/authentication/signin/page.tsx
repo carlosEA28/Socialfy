9abcbbe927bfd1e2b843365a86d3ae32b63190b1
carlosEA28 <carloseaduarte28@gmail.com>
2025-09-18 21:28:54 -0300

    feat: add initial login page

---
+++ src/app/authentication/signin/page.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import Link from "next/link";
+
+const SigninPage = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <div className="flex flex-col gap-20">
+        <div className="text-center">
+          <h1 className="font-medium text-[54px] text-[#FAFAFA]">
+            Login to Your Account
+          </h1>
+          <p className=" text-lg text-[#71717A]">
+            Welcome back! Enter your account to access
+          </p>
+        </div>
+
+        <div className="flex gap-52 ">
+          <div className="flex flex-col items-center justify-center p-4 gap-4 w-[440px] h-[267px] ">
+            <Input
+              type="text"
+              className=" bg-[#222222] border-none  h-[72px]  text-[#898889] placeholder:text-[#898889] font-medium text-lg"
+              placeholder="Email"
+            />
+            <Input
+              type="password"
+              className="bg-[#222222] border-none h-[72px] text-[#898889] placeholder:text-[#898889] font-medium text-lg"
+              placeholder="8 Digit Pin"
+            />
+            <Button className="w-full h-[72px] cursor-pointer" size={"lg"}>
+              Login to Your Account
+            </Button>
+          </div>
+
+          <div className="flex flex-col  p-4 items-center gap-4 w-[440px] h-[267px]">
+            <div className="text-center">
+              <h3 className="font-medium text-lg text-[#FAFAFA]">
+                Want to login with your social media account ?
+              </h3>
+
+              <p className=" text-lg text-[#71717A]">
+                Choose one of the available social logins
+              </p>
+            </div>
+            <Button className="w-full h-[72px]" asChild>
+              <Link href={"#"}>Go to social</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SigninPage;
